dd56ec2a6013643587c23c9890a06c757c7128b0
Rafael Felipe <rafafelipesilva@gmail.com>
2017-11-10 00:44:07 -0200

    Navigation

---
+++ src/pages/Search.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default class Search extends React.Component {
+	render() {
+		return (
+			<View>
+				<Text>Search</Text>
+			</View>
+		);
+	}
+}
